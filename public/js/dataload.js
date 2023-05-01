fa7d925fd05148f820bc92dfd15faf998987180e
Junstory <lastcom1@gmail.com>
2023-05-02 00:37:03 +0900

    DB 읽기 완료 지도에 마커표시 진행중

---
+++ public/js/dataload.js
@@ -0,0 +1,44 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoZUGl18Dhh0nKli7WsGjYzDRYSTdoEZA",
+  authDomain: "khufoodmap.firebaseapp.com",
+  databaseURL: "https://khufoodmap-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "khufoodmap",
+  storageBucket: "khufoodmap.appspot.com",
+  messagingSenderId: "476679246747",
+  appId: "1:476679246747:web:57414e0c6459a9c927873e",
+  measurementId: "G-CT06N9E2YG"
+};
+
+firebase.initializeApp(firebaseConfig);
+database = firebase.database();
+
+//Data Load
+export default positions = [];
+//name : loadStores
+function loadStores(){
+    console.log("입력시작");
+    database.ref("stores").on("value", callback);
+    function callback(snapshot){
+
+        snapshot.forEach(function(child){
+            let info = child.val();
+             let temp = {
+               content: '<div>'+ info.store_name + "</div>",
+               latlng: new kakao.maps.LatLng(info.loc_lat,info.loc_lng)
+             };
+             positions.push(temp);
+        });
+    }
+}
+loadStores();
+
+
+let temp = {
+  content: '<div>'+ "테스트" + "</div>",
+  latlng: new kakao.maps.LatLng(37.247899117265305, 127.07816984215327)
+};
+positions.push(temp);
+
+console.log(positions);
+console.log("입력 완료");
--- public/js/datashow.js
+++ public/js/datashow.js
@@ -1,18 +1,4 @@
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAoZUGl18Dhh0nKli7WsGjYzDRYSTdoEZA",
-  authDomain: "khufoodmap.firebaseapp.com",
-  databaseURL: "https://khufoodmap-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "khufoodmap",
-  storageBucket: "khufoodmap.appspot.com",
-  messagingSenderId: "476679246747",
-  appId: "1:476679246747:web:57414e0c6459a9c927873e",
-  measurementId: "G-CT06N9E2YG"
-};
-
-firebase.initializeApp(firebaseConfig);
-database = firebase.database();
-
+import positions from "./dataload.js";
 //카카오맵
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
@@ -22,38 +8,9 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 console.log("지도생성 성공");
-//Data Load
-let positions = [];
-function loadStores(){
-    console.log("입력시작");
-    database.ref("stores").on("value", callback);
-    function callback(snapshot){
-        console.log(snapshot);
-        snapshot.forEach(function(child){
-             console.log('<div>'+ child.store_name + "</div>");
-             let temp = {
-               content: '<div>'+ child.store_name + "</div>",
-               latlng: new kakao.maps.LatLng(child.loc_lat,child.loc_lng)
-             };
-             console.log(temp);
-             positions.push(temp);
-        });
-        $("#chatlist").scrollTop(15000);
-    }
-}
-console.log("입력 완료");
 
-let temp = {
-  content: '<div>'+ "테스트" + "</div>",
-  latlng: new kakao.maps.LatLng(37.247899117265305, 127.07816984215327)
-};
-positions.push(temp);
-
-console.log(positions);
 
 // 지도를 클릭한 위치에 표출할 마커입니다
-
-
 for (var i = 0; i < positions.length; i ++) {
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
@@ -66,6 +23,7 @@ for (var i = 0; i < positions.length; i ++) {
         content: positions[i].content // 인포윈도우에 표시할 내용
     });
     console.log("윈도우 생성완료");
+    console.log(positions.length);
 
     // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
     // 이벤트 리스너로는 클로저를 만들어 등록합니다
@@ -73,6 +31,7 @@ for (var i = 0; i < positions.length; i ++) {
     kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
     kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
 }
+
 
 // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
 function makeOverListener(map, marker, infowindow) {
